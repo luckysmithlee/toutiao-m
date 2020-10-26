a62d50cfeed828adcb5bf7bdd40eaf43ac581582
Lee-smith-hero <390026008@qq.com>
2020-10-26 20:54:20 +0800

    登录注册-页面布局-样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,41 @@
 <template>
-  <div class="login-container">登录页面</div>
+  <div class="login-container">
+    <van-nav-bar
+      class="app-nav-bar"
+      title="注册/登录"
+
+      left-arrow
+      @click-left="$router.back()"
+    />
+    <!-- 登陆表单 -->
+    <van-cell-group>
+      <van-field
+        v-model="user.moble"
+        icon-prefix="toutiao"
+        left-icon="shouji"
+        placeholder="请输入手机号"
+      />
+      <van-field
+        v-model="user.code"
+        clearable
+        icon-prefix="toutiao"
+        left-icon="yanzhengma"
+        placeholder="请输入验证码"
+      >
+        <template #button>
+          <van-button
+          size="mini"
+          round
+          class="send-btn"
+          >发送验证码</van-button>
+        </template>
+      </van-field>
+    </van-cell-group>
+    <div class = "login-btn-wrap">
+    <van-button class="login-btn" type="info" block>登录</van-button>
+    </div>
+    <!-- /登陆表单 -->
+  </div>
 </template>
 
 <script>
@@ -8,7 +44,12 @@ export default {
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      user: {
+        mobile: '', // 手机号
+        code: '' // 验证码
+      }
+    }
   },
   computed: {},
   watch: {},
@@ -24,5 +65,25 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+.login-container{
+  .login-btn-wrap{
+    padding: 26px 16px;
+    .login-btn{
+      background-color: #6db4fb;
+      border: none;
+      .van-button__text{
+        font-size: 15px;
+      }
+    }
+  }
+  .send-btn{
+    width: 76px;
+    height: 23px;
+    background-color: #ededed;
+    .van-button__text{
+      font-size: 11px;
+      color: #666666;
+    }
+  }
+}
 </style>
